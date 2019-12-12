50409c350c76907c98f4a13da4f3e8d228f2a55a
peter8995 <peter3929293@gmail.com>
2019-12-12 11:27:36 +0800

    complete all

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,70 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//addr=SCREEN
+//n=8192
+//if(kbd!=0){
+//    for(i=0;i<n;i++){
+//        addr[i]=-1
+//    }
+//}
+//else{
+//    addr[i]=0
+//}
+(FOREVER)    
+    //addr=SCREEN
+    @SCREEN
+    D=A
+    @addr
+    M=D
+    //n=8192
+    @8192
+    D=A
+    @n
+    M=D
+
+    @KBD
+    D=M
+    @FILL
+    D;JGT
+    @UNFILL
+    0;JMP
+
+(FILL)
+    @i
+    M=0
+    (FILLLOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @FOREVER
+    D;JEQ
+    @addr
+    D=M
+    @i
+    A=D+M
+    M=-1
+    @i
+    M=M+1
+    @FILLLOOP
+    0;JMP
+(UNFILL)
+    @i
+    M=0
+    (UNFILLLOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @FOREVER
+    D;JEQ
+    @addr
+    D=M
+    @i
+    A=D+M
+    M=0
+    @i
+    M=M+1
+    @UNFILLLOOP
+    0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//int main() {
+//    int R0 = 3;
+//    int R1 = 5;
+//    int R2 = 0;
+//    
+//    while (R0 > 0) {    loop
+//      R2 = R2 + R1;
+//      R0 = R0 - 1;
+//      printf("R0=%d R1=%d R2=%d\n", R0, R1, R2);
+//    }
+//    
+//    printf("R2=%d\n", R2);
+//}
+    @2
+    M=0
+    
+
+(LOOP)
+    @0
+    D=M
+    @END
+    D;JLE
+
+    @1
+    D=M
+    @2
+    M=D+M
+    @0
+    M=M-1
+    @LOOP
+    0;JMP
+    
+    @END
+(END)
+    @END
+    0;JMP
